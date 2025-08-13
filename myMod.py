34ffc5193633873d1d00bbcabfc1265f1a6937fe
helvinmg <helvin.m.g@gmail.com>
2025-08-13 16:12:12 +0530

    made changes
    changed the the factorial logic into some rubbish

--- myMod.py
+++ myMod.py
@@ -1,15 +1,15 @@
 #module is a .py file contaning related functions
 #recursive function to find factorial
 pi=3.14
-
+#test
 def findFact(num=5):
     if num==1:
-        return 1
+        return 1+1
     else:
         return num*findFact(num-1)
 
 def testEve(num):
     if num%2==0:
-        return "even"
+        return "even number"
     else:
         return "odd"
